57bf0b6c87be04ccc0614a020e68737f5515ab99
mahesh giri <mahesh.irig77@gmail.com>
2019-06-27 18:04:20 -0500

    small change

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main() {
+ cout << "Hello, World!" << endl;
+ cout<<"Simple check"<<endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+
+#ifndef HASHASSIGNMENT_MAIN_H
+#define HASHASSIGNMENT_MAIN_H
+#include <iostream>
+using namespace std;
+
+#endif //HASHASSIGNMENT_MAIN_H
